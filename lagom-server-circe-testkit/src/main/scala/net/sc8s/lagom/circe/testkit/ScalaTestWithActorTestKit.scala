10c5acdaf26364679a7af84613d792080ff08d8c
Andreas Gabor <andreas@rob.ag>
2021-02-17 14:59:29 +0100

    load default config too

--- lagom-server-circe-testkit/src/main/scala/net/sc8s/lagom/circe/testkit/ScalaTestWithActorTestKit.scala
+++ lagom-server-circe-testkit/src/main/scala/net/sc8s/lagom/circe/testkit/ScalaTestWithActorTestKit.scala
@@ -3,12 +3,16 @@ package net.sc8s.lagom.circe.testkit
 import akka.actor.testkit.typed.scaladsl.ActorTestKit.ApplicationTestConfig
 import akka.actor.typed.scaladsl.adapter.ClassicActorSystemOps
 import akka.persistence.testkit.{PersistenceTestKitPlugin, PersistenceTestKitSnapshotPlugin}
+import com.typesafe.config.ConfigFactory
 import net.sc8s.akka.circe.CirceSerializerRegistry
 import net.sc8s.lagom.circe.ActorSystemProvider
 import play.api.Environment
 
 abstract class ScalaTestWithActorTestKit(circeSerializerRegistry: CirceSerializerRegistry) extends akka.actor.testkit.typed.scaladsl.ScalaTestWithActorTestKit(ActorSystemProvider.start(
-  PersistenceTestKitPlugin.config.withFallback(PersistenceTestKitSnapshotPlugin.config).withFallback(ApplicationTestConfig),
+  PersistenceTestKitPlugin.config
+    .withFallback(PersistenceTestKitSnapshotPlugin.config)
+    .withFallback(ApplicationTestConfig)
+    .withFallback(ConfigFactory.load()),
   Environment.simple(),
   circeSerializerRegistry
 ).toTyped)

b85a7f735128e11ddb3b52e487d2745a84bbae0f
Andreas Gabor <andreas@rob.ag>
2021-06-04 11:22:41 +0200

    prefix version variables with artifactId to help scala-steward bumping the versions

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,39 +10,37 @@ object Dependencies {
 
   object play {
     // same as lagom uses
-    private val version = "2.8.4"
-    val core = "com.typesafe.play" %% "play" % version
+    val core = "com.typesafe.play" %% "play" % "2.8.4"
   }
 
   object lagom {
-    private val version = "1.6.4"
-    val scaladslServer = "com.lightbend.lagom" %% "lagom-scaladsl-server" % version
-    val scaladslApi = "com.lightbend.lagom" %% "lagom-scaladsl-api" % version
+    private val lagomVersion = "1.6.4"
+    val scaladslServer = "com.lightbend.lagom" %% "lagom-scaladsl-server" % lagomVersion
+    val scaladslApi = "com.lightbend.lagom" %% "lagom-scaladsl-api" % lagomVersion
 
     object js {
-      private val version = "0.4.0-1.6.4"
+      private val lagomJsVersion = "0.4.0-1.6.4"
 
-      val scalaDslApi = Def.setting("com.github.mliarakos.lagomjs" %%% "lagomjs-scaladsl-api" % version)
-      val scalaDslClient = Def.setting("com.github.mliarakos.lagomjs" %%% "lagomjs-scaladsl-client" % version)
+      val scalaDslApi = Def.setting("com.github.mliarakos.lagomjs" %%% "lagomjs-scaladsl-api" % lagomJsVersion)
+      val scalaDslClient = Def.setting("com.github.mliarakos.lagomjs" %%% "lagomjs-scaladsl-client" % lagomJsVersion)
     }
   }
 
   object akka {
-    private val version = "2.6.11"
-    val jsVersion = "2.2.6.9"
-    private val httpVersion = "10.1.13"
+    private val akkaVersion = "2.6.11"
+    private val akkaHttpVersion = "10.1.13"
 
-    val actor = "com.typesafe.akka" %% "akka-actor" % version
-    val stream = "com.typesafe.akka" %% "akka-stream" % version
-    val http = "com.typesafe.akka" %% "akka-http" % httpVersion
-    val typed = "com.typesafe.akka" %% "akka-actor-typed" % version
-    val persistenceTyped = "com.typesafe.akka" %% "akka-persistence-typed" % version
-    val persistenceTestkit = "com.typesafe.akka" %% "akka-persistence-testkit" % version
+    val actor = "com.typesafe.akka" %% "akka-actor" % akkaVersion
+    val stream = "com.typesafe.akka" %% "akka-stream" % akkaVersion
+    val http = "com.typesafe.akka" %% "akka-http" % akkaHttpVersion
+    val typed = "com.typesafe.akka" %% "akka-actor-typed" % akkaVersion
+    val persistenceTyped = "com.typesafe.akka" %% "akka-persistence-typed" % akkaVersion
+    val persistenceTestkit = "com.typesafe.akka" %% "akka-persistence-testkit" % akkaVersion
     val persistenceCassandra = "com.typesafe.akka" %% "akka-persistence-cassandra" % "1.0.4"
-    val streamTyped = "com.typesafe.akka" %% "akka-stream-typed" % version
-    val testkit = "com.typesafe.akka" %% "akka-testkit" % version
-    val testkitTyped = "com.typesafe.akka" %% "akka-actor-testkit-typed" % version
-    val streamTestkit = "com.typesafe.akka" %% "akka-stream-testkit" % version % Test
+    val streamTyped = "com.typesafe.akka" %% "akka-stream-typed" % akkaVersion
+    val testkit = "com.typesafe.akka" %% "akka-testkit" % akkaVersion
+    val testkitTyped = "com.typesafe.akka" %% "akka-actor-testkit-typed" % akkaVersion
+    val streamTestkit = "com.typesafe.akka" %% "akka-stream-testkit" % akkaVersion % Test
 
     val httpCirce = "de.heikoseeberger" %% "akka-http-circe" % "1.35.3"
 
@@ -52,7 +50,7 @@ object Dependencies {
       "akka-http-spray-json",
       "akka-parsing",
       "akka-http-xml"
-    ).map("com.typesafe.akka" %% _ % httpVersion) ++
+    ).map("com.typesafe.akka" %% _ % akkaHttpVersion) ++
       Seq(
         "akka-actor",
         "akka-actor-testkit-typed",
@@ -73,37 +71,36 @@ object Dependencies {
         "akka-serialization-jackson",
         "akka-slf4j",
         "akka-stream"
-      ).map("com.typesafe.akka" %% _ % version)
+      ).map("com.typesafe.akka" %% _ % akkaVersion)
   }
 
   object macwire {
-    val version = "2.3.7"
+    val macwireVersion = "2.3.7"
 
-    val macros = "com.softwaremill.macwire" %% "macros" % version % "provided"
-    val macrosAkka = "com.softwaremill.macwire" %% "macrosakka" % version % "provided"
-    val util = "com.softwaremill.macwire" %% "util" % version
+    val macros = "com.softwaremill.macwire" %% "macros" % macwireVersion % "provided"
+    val macrosAkka = "com.softwaremill.macwire" %% "macrosakka" % macwireVersion % "provided"
+    val util = "com.softwaremill.macwire" %% "util" % macwireVersion
   }
 
   object circe {
-    private val version = "0.13.0"
+    private val circeVersion = "0.13.0"
 
-    val core = Def.setting("io.circe" %%% "circe-core" % version)
-    val generic = Def.setting("io.circe" %%% "circe-generic" % version)
-    val genericExtras = Def.setting("io.circe" %%% "circe-generic-extras" % version)
-    val parser = Def.setting("io.circe" %%% "circe-parser" % version)
+    val core = Def.setting("io.circe" %%% "circe-core" % circeVersion)
+    val generic = Def.setting("io.circe" %%% "circe-generic" % circeVersion)
+    val genericExtras = Def.setting("io.circe" %%% "circe-generic-extras" % circeVersion)
+    val parser = Def.setting("io.circe" %%% "circe-parser" % circeVersion)
   }
 
   object logstage {
-    private val version = "1.0.3"
+    private val izumiVersion = "1.0.3"
 
-    val core = Def.setting("io.7mind.izumi" %%% "logstage-core" % version)
-    val circe = Def.setting("io.7mind.izumi" %%% "logstage-rendering-circe" % version)
-    val fromSlf4j = "io.7mind.izumi" %% "logstage-adapter-slf4j" % version
-    val toSlf4j = "io.7mind.izumi" %% "logstage-sink-slf4j" % version
+    val core = Def.setting("io.7mind.izumi" %%% "logstage-core" % izumiVersion)
+    val circe = Def.setting("io.7mind.izumi" %%% "logstage-rendering-circe" % izumiVersion)
+    val fromSlf4j = "io.7mind.izumi" %% "logstage-adapter-slf4j" % izumiVersion
+    val toSlf4j = "io.7mind.izumi" %% "logstage-sink-slf4j" % izumiVersion
   }
 
   object cats {
-    private val version = "2.2.0"
-    val core = Def.setting("org.typelevel" %%% "cats-core" % version)
+    val core = Def.setting("org.typelevel" %%% "cats-core" % "2.2.0")
   }
 }

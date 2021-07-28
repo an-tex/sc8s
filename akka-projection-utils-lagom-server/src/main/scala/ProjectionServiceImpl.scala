e571091da4066b7c3a1fddc68b871171780d1281
Andreas Gabor <andreas@rob.ag>
2021-07-28 16:21:59 +0200

    use executioncontext of provided actorSystem

--- akka-projection-utils-lagom-server/src/main/scala/ProjectionServiceImpl.scala
+++ akka-projection-utils-lagom-server/src/main/scala/ProjectionServiceImpl.scala
@@ -7,13 +7,14 @@ import com.lightbend.lagom.scaladsl.api.ServiceCall
 import com.lightbend.lagom.scaladsl.api.transport.BadRequest
 import net.sc8s.akka.projection.ProjectionUtils.ManagedProjection
 
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.Future
 
 trait ProjectionServiceImpl extends ProjectionService {
   val projections: Set[ManagedProjection[_, _]]
 
-  implicit val typedSystem: ActorSystem[_]
-  implicit val executionContext: ExecutionContext
+  implicit val actorSystem: ActorSystem[_]
+
+  import actorSystem.executionContext
 
   override def rebuildProjection(projectionName: String) = ServiceCall { _ =>
     projections.find(_.projectionName == projectionName) match {

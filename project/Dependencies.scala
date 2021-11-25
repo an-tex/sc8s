a8da8e72b637fc8ad9dcc05b486e430d7516dd67
Scala Steward <me@scala-steward.org>
2021-11-26 00:04:07 +0100

    Update play to 2.8.9

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -14,7 +14,7 @@ object Dependencies {
 
   object play {
     // same as lagom uses
-    val core = "com.typesafe.play" %% "play" % "2.8.8"
+    val core = "com.typesafe.play" %% "play" % "2.8.9"
   }
 
   object lagom {

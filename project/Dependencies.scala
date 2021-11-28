2c39d14f53b1de470ea07cd182b1670bdfb5fd7d
Scala Steward <me@scala-steward.org>
2021-11-28 06:05:25 +0100

    Update cats-core to 2.7.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -118,6 +118,6 @@ object Dependencies {
   }
 
   object cats {
-    val core = Def.setting("org.typelevel" %%% "cats-core" % "2.6.1")
+    val core = Def.setting("org.typelevel" %%% "cats-core" % "2.7.0")
   }
 }

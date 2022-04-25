c6dbfbfc03c738c69c67724a8fad197a39947bde
Scala Steward <me@scala-steward.org>
2022-04-25 09:07:00 +0200

    Update scalatest to 3.2.12

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   val scala213 = "2.13.8"
 
   val chimney = Def.setting("io.scalaland" %%% "chimney" % "0.6.1")
-  val scalaTest = Def.setting("org.scalatest" %%% "scalatest" % "3.2.10")
+  val scalaTest = Def.setting("org.scalatest" %%% "scalatest" % "3.2.12")
   val scalamock = "org.scalamock" %% "scalamock" % "5.2.0" % Test
   val slf4j = "org.slf4j" % "slf4j-api" % "1.7.36"
   val scalaJavaTime = Def.setting("io.github.cquiroz" %%% "scala-java-time" % "2.3.0")

b7a9078cb7bb8f87321020a4c9569ae8caea528c
Scala Steward <me@scala-steward.org>
2022-05-12 00:04:20 +0200

    Update akka-projection-cassandra, ... to 1.2.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -81,7 +81,7 @@ object Dependencies {
       ).map("com.typesafe.akka" %% _ % akkaVersion)
 
     object projection {
-      private val projectionVersion = "1.2.3"
+      private val projectionVersion = "1.2.4"
 
       val eventsourced = "com.lightbend.akka" %% "akka-projection-eventsourced" % projectionVersion
       val cassandra = "com.lightbend.akka" %% "akka-projection-cassandra" % projectionVersion

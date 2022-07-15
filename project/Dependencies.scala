a0c3cb80e983dd91e53a8f0f4cb25757275803ff
Scala Steward <scala_steward@virtuslab.com>
2022-07-15 12:23:10 +0000

    Update chimney to 0.6.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ import sbt._
 object Dependencies {
   val scala213 = "2.13.8"
 
-  val chimney = Def.setting("io.scalaland" %%% "chimney" % "0.6.1")
+  val chimney = Def.setting("io.scalaland" %%% "chimney" % "0.6.2")
   val scalaTest = Def.setting("org.scalatest" %%% "scalatest" % "3.2.10")
   val scalamock = "org.scalamock" %% "scalamock" % "5.2.0" % Test
   val slf4j = "org.slf4j" % "slf4j-api" % "1.7.36"

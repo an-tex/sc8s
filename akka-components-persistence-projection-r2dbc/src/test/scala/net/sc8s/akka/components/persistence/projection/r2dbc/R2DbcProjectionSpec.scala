3f93eee3d37c5f7bd78cd565bf665f6b62226783
Andreas Gabor <andreas@rob.ag>
2022-12-01 16:28:46 +0100

    add r2dbc singleton projection

--- akka-components-persistence-projection-r2dbc/src/test/scala/net/sc8s/akka/components/persistence/projection/r2dbc/R2DbcProjectionSpec.scala
+++ akka-components-persistence-projection-r2dbc/src/test/scala/net/sc8s/akka/components/persistence/projection/r2dbc/R2DbcProjectionSpec.scala
@@ -25,46 +25,45 @@ class R2DbcProjectionSpec extends ScalaTestWithActorTestKit(ConfigFactory.parseS
     |""".stripMargin)) with AnyFreeSpecLike with Matchers {
 
   "ClusterComponents" - {
-    // TODO https://discuss.lightbend.com/t/r2dbc-eventsbyslices-query-for-projections-with-cluster-singleton-without-entitytype/10089
-    //"Singleton with projections" in {
-    //  object ComponentObject extends ClusterComponent.Singleton.EventSourced with ClusterComponent.SameSerializableCommand {
-    //    case class Command()
-    //    implicit val commandCodec: Codec[SerializableCommand] = deriveCodec
-    //
-    //    case class Event()
-    //    implicit val eventCodec: Codec[Event] = deriveCodec
-    //
-    //    case class State()
-    //
-    //    class Component(dependency: Dependency) extends BaseComponent with R2dbcProjection {
-    //      override val behavior = componentContext => EventSourcedBehavior(
-    //        componentContext.persistenceId,
-    //        State(),
-    //        {
-    //          case (state, command) => Effect.none
-    //        },
-    //        {
-    //          case (state, event) => state
-    //        })
-    //
-    //      override val projections = Set(
-    //        ClusterComponent.Projection(
-    //          "projectionSingleton",
-    //          {
-    //            case (event, projectionContext) => Future.successful(Done)
-    //          }
-    //        ))
-    //    }
-    //
-    //    override val name = "singleton"
-    //
-    //    override val commandSerializer = CirceSerializer()
-    //
-    //    override val eventSerializer = CirceSerializer()
-    //  }
-    //
-    //  ComponentObject.init(new ComponentObject.Component(new Dependency)).delayedInit()
-    //}
+    "Singleton with projections" in {
+      object ComponentObject extends ClusterComponent.Singleton.EventSourced with ClusterComponent.SameSerializableCommand {
+        case class Command()
+        implicit val commandCodec: Codec[SerializableCommand] = deriveCodec
+
+        case class Event()
+        implicit val eventCodec: Codec[Event] = deriveCodec
+
+        case class State()
+
+        class Component(dependency: Dependency) extends BaseComponent with R2dbcSingletonProjection {
+          override val behavior = componentContext => EventSourcedBehavior(
+            componentContext.persistenceId,
+            State(),
+            {
+              case (state, command) => Effect.none
+            },
+            {
+              case (state, event) => state
+            })
+
+          override val projections = Set(
+            ClusterComponent.Projection(
+              "projectionSingleton",
+              {
+                case (event, projectionContext) => Future.successful(Done)
+              }
+            ))
+        }
+
+        override val name = "singleton"
+
+        override val commandSerializer = CirceSerializer()
+
+        override val eventSerializer = CirceSerializer()
+      }
+
+      ComponentObject.init(new ComponentObject.Component(new Dependency)).delayedInit()
+    }
     "Sharded with projections" in {
       object ComponentObject extends ClusterComponent.Sharded.EventSourced.WithSnapshots with ClusterComponent.SameSerializableCommand with ClusterComponent.Sharded.StringEntityId {
         case class Command()
@@ -76,7 +75,7 @@ class R2DbcProjectionSpec extends ScalaTestWithActorTestKit(ConfigFactory.parseS
         case class State()
         implicit val stateCodec: Codec[State] = deriveCodec
 
-        class Component(dependency: Dependency) extends BaseComponent with R2dbcProjection {
+        class Component(dependency: Dependency) extends BaseComponent with R2dbcShardedProjection {
           override val behavior = componentContext => EventSourcedBehavior(
             componentContext.persistenceId,
             State(),

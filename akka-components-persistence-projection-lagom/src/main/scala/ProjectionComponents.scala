a38d977d93ddc1292540df6a7944704a5d88ec59
Andreas Gabor <andreas@rob.ag>
2022-12-05 13:12:17 +0100

    wire managed projections

--- akka-components-persistence-projection-lagom/src/main/scala/ProjectionComponents.scala
+++ akka-components-persistence-projection-lagom/src/main/scala/ProjectionComponents.scala
@@ -2,14 +2,15 @@ package net.sc8s.akka.components.persistence.projection.lagom
 
 import com.lightbend.lagom.scaladsl.server.LagomApplication
 import net.sc8s.akka.circe.{CirceSerializer, CirceSerializerRegistry}
+import net.sc8s.akka.components.lagom.WiredClusterComponents
 import net.sc8s.akka.components.persistence.projection.ManagedProjection
 import net.sc8s.akka.components.persistence.projection.api.ProjectionService.ProjectionStatus
 import net.sc8s.lagom.circe.CirceAkkaSerializationComponents
 
 trait ProjectionComponents extends CirceAkkaSerializationComponents {
-  _: LagomApplication =>
+  _: LagomApplication with WiredClusterComponents =>
 
-  def projections: Set[ManagedProjection[_]] = Set.empty
+  lazy val projections: Set[ManagedProjection[_]] = clusterComponents.flatMap(_.managedProjections)
 
   override def circeSerializerRegistry = super.circeSerializerRegistry ++ new CirceSerializerRegistry {
     override def serializers = Seq(CirceSerializer[ProjectionStatus]())

41c6d4984bdee0a17d3630262d03c801c1c4696b
Scala Steward <scala_steward@virtuslab.com>
2023-09-13 13:13:49 +0000

    Update scala-library to 2.13.12

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import org.portablescala.sbtplatformdeps.PlatformDepsPlugin.autoImport._
 import sbt._
 
 object Dependencies {
-  val scala213 = "2.13.11"
+  val scala213 = "2.13.12"
 
   val scalaTest = Def.setting("org.scalatest" %%% "scalatest" % "3.2.17")
   val specs2 = Def.setting("org.scalatest" %%% "scalatest" % "3.2.17")

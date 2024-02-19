0870707a5776f3a29a8cdc5e70ae6ed0372b4360
Scala Steward <scala_steward@virtuslab.com>
2024-02-19 14:06:11 +0000

    Update logback-classic, logback-core to 1.5.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -144,7 +144,7 @@ object Dependencies {
   }
 
   object logback {
-    private val logback = "1.4.14"
+    private val logback = "1.5.0"
 
     val core = "ch.qos.logback" % "logback-core" % logback
     val classic = "ch.qos.logback" % "logback-classic" % logback

0a393854b2129f9fb459d93de2630f3b9669ddd7
Scala Steward <scala_steward@virtuslab.com>
2024-03-26 14:21:56 +0000

    Update framework to 8.13.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -152,7 +152,7 @@ object Dependencies {
 
   object elastic4s {
     private val elastic4s = "8.11.5"
-    private val elasticsearch = "8.12.2"
+    private val elasticsearch = "8.13.0"
 
     val clientAkka = "com.sksamuel.elastic4s" %% "elastic4s-client-akka" % elastic4s
     val clientJava = "com.sksamuel.elastic4s" %% "elastic4s-client-esjava" % elastic4s

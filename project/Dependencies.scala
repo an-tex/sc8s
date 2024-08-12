dd530fffa8687ec4b534fb48908f56cef53d3687
Scala Steward <scala_steward@virtuslab.com>
2024-08-12 11:25:08 +0000

    Update slf4j-api to 2.0.16

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
   val scalaTest = Def.setting("org.scalatest" %%% "scalatest" % "3.2.19")
   val specs2 = Def.setting("org.scalatest" %%% "scalatest" % "3.2.19")
   val scalamock = "org.scalamock" %% "scalamock" % "6.0.0" % Test
-  val slf4j = "org.slf4j" % "slf4j-api" % "2.0.13"
+  val slf4j = "org.slf4j" % "slf4j-api" % "2.0.16"
   val scalaJavaTime = Def.setting("io.github.cquiroz" %%% "scala-java-time" % "2.6.0")
   val scalaXml = "org.scala-lang.modules" %% "scala-xml" % "2.1.0"
   val nameOf = "com.github.dwickern" %% "scala-nameof" % "3.0.0-antex"

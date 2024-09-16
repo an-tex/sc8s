809edb01c07e0b27ff7e3e9b0197d6f06e5a3aa3
Scala Steward <scala_steward@virtuslab.com>
2024-09-16 13:16:28 +0000

    Update macros to 2.6.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -131,7 +131,7 @@ object Dependencies {
   }
 
   object macwire {
-    val macwireVersion = "2.6.1"
+    val macwireVersion = "2.6.2"
 
     val macros = "com.softwaremill.macwire" %% "macros" % macwireVersion % "provided"
     val macrosAkka = "com.softwaremill.macwire" %% "macrosakka" % macwireVersion % "provided"
